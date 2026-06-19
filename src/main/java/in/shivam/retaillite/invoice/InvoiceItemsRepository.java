e3ed74cb8ec1c22fededefc9a1974db40786c156
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-06-19 12:14:41 +0530

    moved this file to repository directory

--- src/main/java/in/shivam/retaillite/invoice/InvoiceItemsRepository.java
+++
@@ -1,7 +0,0 @@
-package in.shivam.retaillite.invoice;
-
-import in.shivam.retaillite.invoice.entity.InvoiceItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceItemsRepository extends JpaRepository<InvoiceItem,Long> {
-}
