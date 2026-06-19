c0b0706af76183b7c0e3b90e6330946e947d3ea7
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-06-19 12:13:52 +0530

    Added features findAll, findInvoice,findByInvoiceStatus

--- src/main/java/in/shivam/retaillite/invoice/service/InvoiceService.java
+++ src/main/java/in/shivam/retaillite/invoice/service/InvoiceService.java
@@ -2,9 +2,21 @@ package in.shivam.retaillite.invoice.service;
 
 import in.shivam.retaillite.invoice.dto.InvoiceRequest;
 import in.shivam.retaillite.invoice.dto.InvoiceResponse;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
 
 public interface InvoiceService {
     InvoiceResponse createInvoice(InvoiceRequest request);
+    Page<InvoiceResponse> findByInvoiceStatus(
+            Integer page,
+            Integer size,
+            String  invoiceStatus
+    );
+    Page<InvoiceResponse> findAll(
+            Integer page,
+            Integer size,
+            String sortBy,
+            String orderedBy
+    );
+
+    InvoiceResponse findInvoice(String invoiceId);
 }
