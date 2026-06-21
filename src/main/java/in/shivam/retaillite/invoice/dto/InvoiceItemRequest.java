a3f21661a071d15b11b74ae2062d2c8832f5a8d2
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-06-21 11:01:20 +0530

    Added validation on Request Data

--- src/main/java/in/shivam/retaillite/invoice/dto/InvoiceItemRequest.java
+++ src/main/java/in/shivam/retaillite/invoice/dto/InvoiceItemRequest.java
@@ -1,7 +1,13 @@
 package in.shivam.retaillite.invoice.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public record InvoiceItemRequest(
+        @NotNull
         String productId,
+        @NotNull
+        @Min(1)
         Integer quantity
 ) {
 }
--- src/main/java/in/shivam/retaillite/invoice/dto/InvoiceRequest.java
+++ src/main/java/in/shivam/retaillite/invoice/dto/InvoiceRequest.java
@@ -1,13 +1,21 @@
 package in.shivam.retaillite.invoice.dto;
 
 import in.shivam.retaillite.common.enums.PaymentMethod;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record InvoiceRequest(
+        @NotNull
         String customerName,
+        @NotNull
         String customerNumber,
+        @Email
         String customerEmail,
+        @NotNull
+        @Min(0)
         List<InvoiceItemRequest> items
 ) {
 }
