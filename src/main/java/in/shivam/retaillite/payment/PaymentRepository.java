54a4d804e45343d481fa890d766582ce9171798b
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-06-27 18:57:33 +0530

    Added findPendingPaymentByInvoiceId

--- src/main/java/in/shivam/retaillite/payment/PaymentRepository.java
+++ src/main/java/in/shivam/retaillite/payment/PaymentRepository.java
@@ -4,4 +4,15 @@ import in.shivam.retaillite.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
+    Optional<Payment> findByInvoice_invoiceIdAndPaymentStatus(String invoiceId, PaymentStatus paymentStatus);
+
+    @Query(
+            value = """
+                    SELECT p from Payment p
+                        JOIN FETCH i.invoice
+                    WHERE p.paymentStatus=PaymentStatus.PENDING
+                          AND i.invoiceId=:invoiceId
+                    ORDER BY p.createdAt DESC"""
+    )
+    Optional<Payment> findPendingPaymentByInvoiceId(String invoiceId);
 }
