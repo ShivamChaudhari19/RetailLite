caf00ece7eb8e9dfda291166248ed8ea322042d6
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-07-21 21:28:00 +0530

    base interface for all integration tests, it includes all required annotations

---
+++ src/test/java/in/shivam/retaillite/integration/config/BaseIntegrationTest.java
@@ -0,0 +1,13 @@
+package in.shivam.retaillite.integration.config;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public abstract class BaseIntegrationTest {
+}
