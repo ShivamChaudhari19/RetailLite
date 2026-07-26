a250ac428ac3151bc175fe4ebdf3fab45b9ad91c
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-07-26 23:29:57 +0530

    application no longer needs dedicated `/decode` endpoint

--- src/main/java/in/shivam/retaillite/auth/controller/AuthController.java
+++ src/main/java/in/shivam/retaillite/auth/controller/AuthController.java
@@ -2,15 +2,15 @@ package in.shivam.retaillite.auth.controller;
 
 import in.shivam.retaillite.auth.dto.AuthRequest;
 import in.shivam.retaillite.auth.dto.AuthResponse;
-import in.shivam.retaillite.auth.dto.RawPassword;
 import in.shivam.retaillite.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -18,18 +18,18 @@ import org.springframework.web.bind.annotation.*;
 
 public class AuthController {
     private final AuthService authService;
-    private final PasswordEncoder encode;
+//    private final PasswordEncoder encode;
     @PreAuthorize(value = "permitAll()")
     @PostMapping("/auth/login")
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthRequest request){
         log.info("Authenticating request: {}",request.getUsername());
         return ResponseEntity.ok(authService.authenticate(request));
     }
-    @PreAuthorize(value = "permitAll()")
-    @PostMapping("/auth/encode")
-    public ResponseEntity<String> encode(@RequestBody RawPassword password)
-    {
-        log.info("Encoding raw password");
-        return ResponseEntity.ok(encode.encode(password.getPassword()));
-    }
+//    @PreAuthorize(value = "permitAll()")
+//    @PostMapping("/auth/encode")
+//    public ResponseEntity<String> encode(@RequestBody RawPassword password)
+//    {
+//        log.info("Encoding raw password");
+//        return ResponseEntity.ok(encode.encode(password.getPassword()));
+//    }
 }
