10cf62037418cc0b038f253a8823e6cacb9fcec0
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-07-27 23:37:40 +0530

    imports improved

--- src/main/java/in/shivam/retaillite/auth/filter/JwtRequestFilter.java
+++ src/main/java/in/shivam/retaillite/auth/filter/JwtRequestFilter.java
@@ -7,25 +7,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
-//@RequiredArgsConstructor
-@Component
 
 public class JwtRequestFilter extends OncePerRequestFilter {
     private static final String BEARER_PREFIX="Bearer ";
