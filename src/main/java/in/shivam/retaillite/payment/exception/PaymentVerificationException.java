8478d157759ec47804e87fad82c01aaa23b688ff
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-07-29 10:25:50 +0530

    added: payment verification Exception

---
+++ src/main/java/in/shivam/retaillite/payment/exception/PaymentVerificationException.java
@@ -0,0 +1,6 @@
+package in.shivam.retaillite.payment.exception;
+
+public class PaymentVerificationException extends RuntimeException {
+    public PaymentVerificationException(String s) { super(s);
+    }
+}
