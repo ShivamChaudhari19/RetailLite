8b66b48d56ed9658726a6d9ee801da63c9715723
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-07-29 10:30:55 +0530

    payment mapper is responseble for creating pending payment only

--- src/main/java/in/shivam/retaillite/payment/mapper/PaymentMapper.java
+++ src/main/java/in/shivam/retaillite/payment/mapper/PaymentMapper.java
@@ -2,9 +2,8 @@ package in.shivam.retaillite.payment.mapper;
 
 import in.shivam.retaillite.common.enums.PaymentStatus;
 import in.shivam.retaillite.invoice.entity.Invoice;
-import in.shivam.retaillite.payment.dto.PaymentRequest;
-import in.shivam.retaillite.payment.dto.PaymentResponse;
-import in.shivam.retaillite.payment.entity.Payment;
+import in.shivam.retaillite.payment.domain.entity.Payment;
+import in.shivam.retaillite.payment.dto.request.PaymentRequest;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -17,30 +16,6 @@ public class PaymentMapper {
                 .invoice(invoice)
                 .paymentMethod(request.paymentMethod())
                 .paymentStatus(PaymentStatus.PENDING)
-                .build();
-    }
-
-    public PaymentResponse toPaymentResponse(Payment savedPayment) {
-        return PaymentResponse.builder()
-                .transactionId(savedPayment.getPaymentId())
-                .invoiceId(savedPayment.getInvoice().getInvoiceId())
-                .paymentMethod(savedPayment.getPaymentMethod())
-                .paymentStatus(savedPayment.getPaymentStatus())
-                .amount(savedPayment.getInvoice().getGrandTotal())
-                .createdAt(savedPayment.getCreatedAt())
-                .build();
-    }
-
-    public Payment toPayment(
-            PaymentRequest request,
-            PaymentStatus paymentStatus,
-            Invoice invoice
-    ) {
-        return Payment.builder()
-                .paymentId(UUID.randomUUID().toString())
-                .invoice(invoice)
-                .paymentMethod(request.paymentMethod())
-                .paymentStatus(paymentStatus)
                 .build();
     }
 }
