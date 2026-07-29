87e9c322ced05768b7b9beea95392e8b6a7322c7
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-07-29 10:24:57 +0530

    added: invoice not paid exception

---
+++ src/main/java/in/shivam/retaillite/payment/exception/InvoiceNotPaidException.java
@@ -0,0 +1,6 @@
+package in.shivam.retaillite.payment.exception;
+
+public class InvoiceNotPaidException extends RuntimeException {
+    public InvoiceNotPaidException(String s) { super(s);
+    }
+}
