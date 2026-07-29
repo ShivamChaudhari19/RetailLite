ce2aa2e5dc42dc77a28c2639baec096a20501707
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-07-29 23:02:22 +0530

    added: active profile test

--- src/test/java/in/shivam/retaillite/invoice/repository/InvoiceRepositoryTest.java
+++ src/test/java/in/shivam/retaillite/invoice/repository/InvoiceRepositoryTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -27,6 +28,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class InvoiceRepositoryTest {
 
     @Autowired
