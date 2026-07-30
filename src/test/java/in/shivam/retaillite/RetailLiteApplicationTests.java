8ed0709c5d4fb79b68fd5c04f474435ddf1c0704
ShivamChaudhari19 <shivamsc7289@gmail.com>
2026-07-30 10:41:47 +0530

    added test as a active profile

--- src/test/java/in/shivam/retaillite/RetailLiteApplicationTests.java
+++ src/test/java/in/shivam/retaillite/RetailLiteApplicationTests.java
@@ -2,8 +2,10 @@ package in.shivam.retaillite;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RetailLiteApplicationTests {
 
     @Test
